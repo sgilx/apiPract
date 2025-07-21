940bf847dcd44c95879e537da9558ac7a4add4f3
Sofia Gil <gx.sofia19@gmail.com>
2025-07-21 16:13:22 -0400

    search by title, shwing selected movie info

--- app.js
+++ app.js
@@ -13,21 +13,91 @@ const fetchData = async (searchTerm) => {
     return response.data.Search;
 };
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+    <label><b>Search for a movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results">
+            </div>
+        </div>
+    </div>
+`;
+
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
 const onInput = async event => {
     const movies = await fetchData(event.target.value);
+
+    if(!movies.length) {
+        dropdown.classList.remove('is-active');
+        return;
+    }
+
+    resultsWrapper.innerHTML = '';
+    dropdown.classList.add('is-active');
     
     for (let movie of movies) {
-        const div = document.createElement('div');
-        
-        div.innerHTML = `
-        <img src="${movie.Poster}" />
-        <h1>${movie.Title}</h1>
+        const option = document.createElement('a');
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+
+        option.classList.add('dropdown-item');
+        option.innerHTML = `
+        <img src="${imgSrc}" />
+        ${movie.Title}
         `;
 
-        document.querySelector('#movieDetails').append(div);
+        option.addEventListener('click', () => {
+            dropdown.classList.remove('is-active');
+            input.value = movie.Title;
+            onMovieSelect(movie);
+        });
+
+        resultsWrapper.append(option);
     }
 };
 
 input.addEventListener('input', debounce(onInput, 500));
+
+document.addEventListener('click', event => {
+    if (!root.contains(event.target)) {
+        dropdown.classList.remove('is-active');
+    }
+});
+
+const onMovieSelect = async movie => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'fb9225d8',
+            i: movie.imdbID
+        }
+    });
+
+    document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = movieDetail => {
+    return `
+    <article class="media">
+        <figure class="media-left">
+        <p class="image">
+            <img src="${movieDetail.Poster}" />
+        </p>
+        </figure>
+        <div class="media-content">
+            <div class="content">
+                <h1>${movieDetail.Title}</h1>
+                <h2>IMDB Rating: ${movieDetail.imdbRating} | MetaScore: ${movieDetail.Metascore}</h2>
+                <h3>${movieDetail.Year}</h3>
+                <h4>${movieDetail.Genre}</h4>
+                <h5>Directed by: ${movieDetail.Director} </h5>
+                <h5>Starring: ${movieDetail.Actors}</h5>
+                <p>${movieDetail.Plot}</p>
+            </div>
+        </div>
+    </article> 
+    `;
+};
