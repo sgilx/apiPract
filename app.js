4f1ff08c7e9166526da12183bbd2a63aacaef4f4
Sofia Gil <gx.sofia19@gmail.com>
2025-07-20 20:29:49 -0400

    search added

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
-fetch('http://www.omdbapi.com/?i=tt3896198&apikey=fb9225d8')
-    .then(res => {
-        console.log(res.json());
-    })
-.catch(err => {
-    console.log(next(err));
-})
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'fb9225d8',
+            s: searchTerm
+        }
+    });
+
+    if (response.data.Error) {
+        return [];
+    }
+
+    return response.data.Search;
+};
+
+const input = document.querySelector('input');
+
+const onInput = async event => {
+    const movies = await fetchData(event.target.value);
+    
+    for (let movie of movies) {
+        const div = document.createElement('div');
+        
+        div.innerHTML = `
+        <img src="${movie.Poster}" />
+        <h1>${movie.Title}</h1>
+        `;
+
+        document.querySelector('#movieDetails').append(div);
+    }
+};
+
+input.addEventListener('input', debounce(onInput, 500));
